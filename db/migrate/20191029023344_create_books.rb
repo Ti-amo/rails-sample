5866bc9fb34fc579f32cd0ee249217c2d8b5a959
Ti-amo <thehungklk@gmail.com>
2019-12-14 22:42:39 +0700

    Make a basic User model (including secure passwords)

--- db/migrate/20191029023344_create_books.rb
+++ db/migrate/20191029023344_create_books.rb
@@ -1,5 +1,6 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
+    drop_table :books
     create_table :books do |t|
       t.string :title
       t.integer :number_of_pages
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_023344) do
+ActiveRecord::Schema.define(version: 2019_12_14_153447) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "number_of_pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
